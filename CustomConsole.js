37cd5f6b44a354f3bf113531866572bcaac81228
Nanana776 <nanabudaghyan06@gmail.com>
2024-10-14 23:31:58 +0400

    history

--- CustomConsole.js
+++ CustomConsole.js
@@ -4,7 +4,15 @@ class CustomConsole{
     }
   log(values){
     const log_text=values.join(' ');
-    this.log.push(log_text);
+    this.logs.push(log_text);
     return log_text;
   }
+  history(start=0, end=this.logs.length){
+    return this.logs.reduce((result, log, idx)=>{
+      if(idx>=start && idx<end){
+        result.push(log);
+      }
+    },[])
+  }
+  
 }
