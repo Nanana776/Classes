8546b7a2aa1b7bd3e6b46c18394e0912c3c3bb3b
Nanana776 <nanabudaghyan06@gmail.com>
2024-10-14 18:50:07 +0400

    dueAmount

--- Coffeeshop.js
+++ Coffeeshop.js
@@ -16,11 +16,30 @@ class CoffeeShop{
    addOrder(itemName){
     const item= this.#menu.find(({item})=> item===itemName);
     if (item){
+        this.#orders.push(item)
         return "Order added!"
     }
     else{
         return " This item is unavailable!"
     }
-
    }
+   fulfillOrder(){
+    if(this.#orders>0) {
+    const done_order=this.#orders.shift();
+    return `The ${done_order} is ready!`
+   }
+   else {
+    return "All orders have been fulfilled!"
+   }
+ }
+ listOrders(){
+    return this.#orders;
+ }
+  dueAmount(){
+    return this.#orders.reduce((total, order) => {
+        const {price}=this.#menu.find(({item}) => item===order);
+        return total+price;
+  },0);
 }
+       }
+  
