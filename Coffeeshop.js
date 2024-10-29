6177417a36ad84ab28c1ae7f01272eb1886e23fa
Nanana776 <nanabudaghyan06@gmail.com>
2024-10-29 19:50:36 +0400

    add and fulfill

--- Coffeeshop.js
+++ Coffeeshop.js
@@ -13,4 +13,24 @@ class CoffeeShop{
    setName(){
     this.#name=name;
    }
+
+   addOrder(itemName){
+    const item= this.#menu.find(({item})=> item===itemName);
+    if (item){
+        this.#orders.push(item)
+        return "Order added!"
+    }
+    else{
+        return " This item is unavailable!"
+    }
+   }
+   fulfillOrder(){
+    if(this.#orders.length > 0) {
+    const done_order=this.#orders.shift();
+    return `The ${done_order.item} is ready!`
+   }
+   else {
+    return "All orders have been fulfilled!"
+   }
+ }
 }
