eab50221b348ee676787ebadba534f2e9b5f7745
Nanana776 <nanabudaghyan06@gmail.com>
2024-10-29 19:52:39 +0400

    list, due, cheap, drink, food

--- Coffeeshop.js
+++ Coffeeshop.js
@@ -33,4 +33,25 @@ class CoffeeShop{
     return "All orders have been fulfilled!"
    }
  }
-}
+
+ listOrders(){
+    return this.#orders.map(order=>order.item);
+ }
+  dueAmount(){
+    return this.#orders.reduce((total, order) => {
+        const {price} = order;
+        return total+price;
+  },0);
+ }
+  cheapestItem(){
+    const cheapest= this.#menu.reduce((minItem, currItem)=>{
+        return (currItem.price < minItem.price)? currItem : minItem},this.#menu[0]);
+        return cheapest.item;
+    }
+    drinksOnly(){
+        return this.#menu.filter(({type})=>type==="drinks").map(({item})=>item);
+    }
+    foodOnly(){
+        return this.#menu.filter(({type})=>type==="food").map(({item})=>item);
+    }
+  }
